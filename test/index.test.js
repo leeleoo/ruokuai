77d7fc5c070f7a7856b01c267be427488e6e9d03
Lee <yang1180mm@126.com>
2016-07-21 09:30:11 +0800

    pwd

--- test/index.test.js
+++ test/index.test.js
@@ -6,7 +6,7 @@ describe('测试图片是否验证成功', function() {
 	it('5568', function(done) {
 		getCaptcha({
 			filePath:path.join(__dirname,'google.png'),
-			password: 'yang1180'
+			password: '******'
 		},function(err,code){
 			expect(code).to.be.equal("5568");
 			done();
